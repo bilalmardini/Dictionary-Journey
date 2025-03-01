3c6c01140fd8715b079172205b0c5daa6829d337
bilal mardini <a220012@students.gu.edu.lb>
2025-03-01 17:44:24 +0200

    counting how many times each word occured

--- Dictionary Journey/DJourney.py
+++ Dictionary Journey/DJourney.py
@@ -3,5 +3,14 @@ def merging_dictionary (dic1, dic2):
     print(dic3)
 dict1 = {"a":1, "b":2, "c":3}
 dict2 = {"b":10,"d":4}
-
 merging_dictionary(dict1,dict2)
+
+
+sentence = input("enter a sentence and repeat some words: ")
+sentenceSplit = sentence.split()
+uniqueWords = set(sentenceSplit)
+
+for word in uniqueWords:
+    counter = sentenceSplit.count(word)
+    print(word+": "+str(counter)+" ") 
+
