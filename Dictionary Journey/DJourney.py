ddf35fdbd510c03838f9a84542b3d252d34e29b3
bilal mardini <a220012@students.gu.edu.lb>
2025-03-01 15:18:22 +0200

    merging 2 dictionaries

--- Dictionary Journey/DJourney.py
+++ Dictionary Journey/DJourney.py
@@ -1,1 +1,7 @@
-print("hello world")
+def merging_dictionary (dic1, dic2):
+    dic3 = {**dic1, **dic2}
+    print(dic3)
+dict1 = {"a":1, "b":2, "c":3}
+dict2 = {"b":10,"d":4}
+
+merging_dictionary(dict1,dict2)
